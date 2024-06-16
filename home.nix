b1c218e82c7bd0de735f7974f446e9340c2c971c
= <41074166+sutheim@users.noreply.github.com>
2024-06-16 22:23:39 +0200

    trying to learn some nix

--- home.nix
+++ home.nix
@@ -1,21 +1,23 @@
 { lib, pkgs, ... }:
-{
-  home = {
-    packages = with pkgs; [
-      neovim
-      git
-      zig
-      rustup
-      unzip
-      sqlite
-      nodejs
-      just
-      dotnet-sdk_8
-    ];
-
+  let
     username = "sondre";
-    homeDirectory = "/home/sondre";
+  in {
+    home = {
+      packages = with pkgs; [
+        neovim
+        git
+        zig
+        rustup
+        unzip
+        sqlite
+        nodejs
+        just
+        dotnet-sdk_8
+      ];
 
-    stateVersion = "23.11";
-  };
+      inherit username;
+      homeDirectory = "/home/${username}";
+
+      stateVersion = "23.11";
+    };
 }
